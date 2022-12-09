32a3c9232412b670a2f4bcb63f1335fb43cc060d
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-12-09 12:06:03 -0700

    Add answers

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,21 +11,21 @@ p "Hello World".downcase
 
 # The include method is called on the string object "Hello World".
 # no arguments passed, include job is to include "Hello" exactly as its defined in arugments
-# return of arguments is fale
- "Hello World".include?("Hello")
+# return of arguments is true since "Hello" is included in the string.
+p  "Hello World".include?("Hello")
 
 # The end_with method is called to on the string object "Hello World" to see if the string ends with the exact
 # combination of letters.  return of argument is False sense this string doesn't end with "hello"
- "Hello World".end_with?("Hello")
+p "Hello World".end_with?("Hello")
 
 # the end_with method is called on the string object "Hello World" to see if the string being called
-# upon ends with the exact 3 letters "rld".  return of argument is True.
- "Hello World".end_with?("rld")
+# upon ends with the exact 3 letters "rld".  return of argument is True because the string that is called on ends with "rld".
+p "Hello World".end_with?("rld")
 
 # the even? method is called on the integer 12 to check if the integer is an even
-# number.  return of argument is True.
- 12.even?
+# number.  return of argument is True because the integer called on is even.
+ p 12.even?
 
 # next method is called on the integer 18 to print out the next highest integer number after 18.
-# should print 19
- 18.next
+# should print 19 since its calling on the integer 18
+ p 18.next
